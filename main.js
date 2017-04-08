bd2094d279ff9f9c89289e660cef04f648bdc004
Osbert0426 <osbertchiang0426@gmail.com>
2017-04-08 14:52:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,14 @@
 //畫布設定
 var bgImg=document.createElement("img");
-
 bgImg.src="images/map.png";
-
 var canvas=document.getElementById("game-canvas");
-
 var ctx=canvas.getContext("2d");
 
+funtion darw(){
+ctx.drawImage(bgImg,0,0);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+draw();
 
 
 
