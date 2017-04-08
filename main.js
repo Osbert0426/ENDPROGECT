834dd6fe6aa913c2545baa2031920d5234592e84
Osbert0426 <osbertchiang0426@gmail.com>
2017-04-08 15:16:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 //畫布設定
 var bgImg=document.createElement("img");
+var enemyImg=document.createElement("img");
 bgImg.src="images/map.png";
 var canvas=document.getElementById("game-canvas");
 var ctx=canvas.getContext("2d");
+
+//set敵人
+var enemy={
+x:96,
+y:448
+
+
+}
 
 function draw(){
 ctx.drawImage(bgImg,0,0);
