a79afbb6304626b66216f53befffb2cf36ed0d73
Osbert0426 <osbertchiang0426@gmail.com>
2017-04-08 15:28:58 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 var bgImg=document.createElement("img");
 var enemyImg=document.createElement("img");
 bgImg.src="images/map.png";
+enemyImg.src="images/jason.gif"
 var canvas=document.getElementById("game-canvas");
 var ctx=canvas.getContext("2d");
 
@@ -9,15 +10,13 @@ var ctx=canvas.getContext("2d");
 var enemy={
 x:96,
 y:448
-
-
 }
-
 function draw(){
 ctx.drawImage(bgImg,0,0);
+ctx.drawImage(enemyImg,enemy.x,enemy.y)
 }
 
-setTimeout(draw,1000);
+setInterval(draw,16);
 
 
 
