5516751b56a6e56b2c74a1d65a724440fedb1d75
Osbert0426 <osbertchiang0426@gmail.com>
2017-04-15 14:06:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 //畫布設定
 var bgImg=document.createElement("img");
 var enemyImg=document.createElement("img");
+var towerbtn=document.createElement("btn");
 bgImg.src="images/map3.png";
 enemyImg.src="images/jason.gif"
+towerbtn.src="images/map3.png";
 var canvas=document.getElementById("game-canvas");
 var ctx=canvas.getContext("2d");
 
@@ -14,9 +16,11 @@ y:448
 function draw(){
 ctx.drawImage(bgImg,0,0);
 ctx.drawImage(enemyImg,enemy.x,enemy.y)
-}
 
+}
 setInterval(draw,16);
 
 
 
+
+ctx.drawImage(towerbtn,560,432,400,50);
