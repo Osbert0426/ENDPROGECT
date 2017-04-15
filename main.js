eb6b81d1401d5637c43ce9b0dac8294861d9791e
Osbert0426 <osbertchiang0426@gmail.com>
2017-04-15 14:55:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,22 +2,43 @@
 var bgImg=document.createElement("img");
 var enemyImg=document.createElement("img");
 var towerbtn=document.createElement("img");
+var drytower=document.createElement("img");
+
 bgImg.src="images/map3.png";
 enemyImg.src="images/jason.gif"
 towerbtn.src="images/tower-btn.png";
+
+
 var canvas=document.getElementById("game-canvas");
 var ctx=canvas.getContext("2d");
+
 
 //set敵人
 var enemy={
 x:96,
 y:448
+};
+
+var cursur={
+x:0,
+y:0
 }
+
+$("#game-canvas").on("mousemove",function(event){
+cursur.x=event.offsetX
+cursur.y=event.offsetY
+})
 function draw(){
 ctx.drawImage(bgImg,0,0);
 ctx.drawImage(enemyImg,enemy.x,enemy.y)
 ctx.drawImage(towerbtn,560,432,100,100);
+ ctx.drawImage(drytower,event.offsetX,event.offsetY,);
+  
+  
 }
 setInterval(draw,16);
+
+
+
 
 
